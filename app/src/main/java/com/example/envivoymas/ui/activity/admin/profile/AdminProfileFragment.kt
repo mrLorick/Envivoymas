bf206c89eb0afe451602553f61994cfb917e6f2c
Lorick <LorickMr@gmail.com>
2022-12-03 17:57:06 +0530

    First Commit 2/Dec Complete Profile screen Admin Done  by Mr Lorick

--- app/src/main/java/com/example/envivoymas/ui/activity/admin/profile/AdminProfileFragment.kt
+++ app/src/main/java/com/example/envivoymas/ui/activity/admin/profile/AdminProfileFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.envivoymas.R
 import com.example.envivoymas.databinding.FragmentAdminProfileBinding
 
@@ -12,11 +13,22 @@ import com.example.envivoymas.databinding.FragmentAdminProfileBinding
 class AdminProfileFragment : Fragment() {
     var binding : FragmentAdminProfileBinding? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_profile, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_admin_profile, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initData()
+    }
+
+    private fun initData() {
+        setToolBar()
+    }
+
+    private fun setToolBar() {
+        binding?.toolbar?.tvTitle?.text = getString(R.string.profile_setting)
     }
 }
